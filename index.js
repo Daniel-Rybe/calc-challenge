b46f4d6f08b8575ccd1e56a92f089d540b70eef2
Daniel Rybe <danilrybakov249@gmail.com>
2021-11-11 03:57:27 +0300

    actually-first-version

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ express = require('express')
 app = express();
 port = process.env.PORT || 3000;
 
+app.use('', express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + 'public/index.html');
 });
 
 app.listen(port);
